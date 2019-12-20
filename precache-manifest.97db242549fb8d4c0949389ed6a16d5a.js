caf0fc897d431fe555e54b65876a1e358b752ab0
Taras <trs.oliynyk@gmail.com>
2019-12-20 23:32:44 +0200

    Updates

---
+++ precache-manifest.97db242549fb8d4c0949389ed6a16d5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cad86068d577dcd664bd098129b3b2",
+    "url": "/react-application-001/index.html"
+  },
+  {
+    "revision": "757aa778c67539fce07a",
+    "url": "/react-application-001/static/css/main.0dec9b1f.chunk.css"
+  },
+  {
+    "revision": "3205a2a8fe8fa00b8026",
+    "url": "/react-application-001/static/js/2.79c5b2bd.chunk.js"
+  },
+  {
+    "revision": "757aa778c67539fce07a",
+    "url": "/react-application-001/static/js/main.90c8ac04.chunk.js"
+  },
+  {
+    "revision": "1e62f3d186a1c242ead9",
+    "url": "/react-application-001/static/js/runtime~main.a167ea39.js"
+  },
+  {
+    "revision": "1a7925ff96284abba19787786c047eb5",
+    "url": "/react-application-001/static/media/loader.1a7925ff.gif"
+  },
+  {
+    "revision": "4f1a60136bc725f8133756ebcb46d94b",
+    "url": "/react-application-001/static/media/logo.4f1a6013.png"
+  }
+]);
